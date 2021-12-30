4269b7646cc4ceb53c72218e9c7e006ad47f0a22
Criptochairo <luis.cardenases@gmail.com>
2021-12-30 17:32:04 -0600

    removed parenthesis for hectors name in Hector component

--- src/Components/BlogSections/Hector.js
+++ src/Components/BlogSections/Hector.js
@@ -11,7 +11,7 @@ function Hector() {
         <div className="text-container">
             <img src={logo} alt="wiisy-logo" id="header-img"/>
             <h1>What If I Say Yes?</h1>
-            <p className="content-text">On November 5, 2005, my brother (Héctor Cárdenas) dedicated a painting to one of his friends. Below you will see the text he read that night. His words inspired me, years later, to create this website in his honor:</p>
+            <p className="content-text">On November 5, 2005, my brother Héctor Cárdenas dedicated a painting to one of his friends. Below you will see the text he read that night. His words inspired me, years later, to create this website in his honor:</p>
             
             <div className="hector">
 
