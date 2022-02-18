84aec7762ef2682c1677533bca5ce22d156ba44b
Criptochairo <luis.cardenases@gmail.com>
2022-02-17 18:00:54 -0600

    even more spelling corrections for podcast/es

--- src/Components/BlogSections/Podcast-es.js
+++ src/Components/BlogSections/Podcast-es.js
@@ -16,11 +16,11 @@ function PodcastEs() {
 
                 <p class="content-text">
 
-                Un día, reflexionando en voz alta con mi esposo y mi hija sobre el sentido de mi vida, y contestando a sus preguntas, llegué a la conclusión de que quería intentar algo nuevo que me daba mucho nervio pero que a la vez me emocionaba enormemente: crear mi propio Pódcast.
+                Un día, reflexionando en voz alta con mi esposo y mi hija sobre el sentido de mi vida, y contestando a sus preguntas, llegué a la conclusión de que quería intentar algo nuevo que me daba mucho nervio pero que a la vez me emocionaba enormemente: crear mi propio pódcast.
                 <br /><br />
-                Gracias a la pandemia había empezado a escuchar muchos Pódcasts y me había enamorado de esta forma tan íntima de comunicación.
+                Gracias a la pandemia había empezado a escuchar muchos pódcasts y me había enamorado de esta forma tan íntima de comunicación.
                 <br /><br />
-                Ahora la pregunta era... ¿y de qué hago mi Pódcast?, ¿qué tengo que ofrecerle al mundo?
+                Ahora la pregunta era... ¿y de qué hago mi pódcast?, ¿qué tengo que ofrecerle al mundo?
                 <br /><br />
                 Pues resulta que desde hace muchos años yo regalo palabras, a mi familia, a mis amigas y amigos queridos, a gente a la que quiero y admiro. Pero esos escritos no los he publicado, no los he compartido con nadie más que con las personas a las que van dedicados.
                 <br /><br />
@@ -28,11 +28,11 @@ function PodcastEs() {
                 <br /><br />
                 Así nació el título, la idea y mi pódcast: <i>De regalos y palabras.</i>
                 <br /><br />
-                El logo me lo diseñó mi hermano gemelo, Jorge. Mi hermano mayor, Luis, es el director de sonido. Mi cuñada, Ana Laura, me ayudó a entrenar mi voz. Y mi hija, Maya, grabó la presentación del Pódcast.
+                El logo me lo diseñó mi hermano gemelo, Jorge. Mi hermano mayor, Luis, es el director de sonido. Mi cuñada, Ana Laura, me ayudó a entrenar mi voz. Y mi hija, Maya, grabó la presentación del pódcast.
                 <br /><br />
                 Mi lengua materna es el español y por eso lo grabo en español. Todavía no me aviento a decir ¿y si digo que sí a grabarlo en inglés? porque eso me da todavía mucho miedo (mi pronunciación no es perfecta y no vaya a ser que acabe diciendo algo que suene a otra cosa, ja, ja). Pero en el espíritu de este sitio web, algún día estaré anunciando que dije que <u><b>¡sí!</b></u> y presentaré mi pódcast en inglés.
                 <br /><br />
-                Mientras tanto, aquí les contaré de lo que hablo en mis distintos episodios, con toda la intención de inspirarlos a hacer lo mismo: aprender a regalar sus propias palabras… y ¿quién sabe? A lo mejor también a crear un día su propio Pódcast.
+                Mientras tanto, aquí les contaré de lo que hablo en mis distintos episodios, con toda la intención de inspirarlos a hacer lo mismo: aprender a regalar sus propias palabras… y ¿quién sabe? A lo mejor también a crear un día su propio pódcast.
                 <br /><br />
 
                 ------------------------<br /><br />
@@ -53,8 +53,8 @@ function PodcastEs() {
                 <ul>
                     <li><a href="https://anchor.fm/lucia-cardenas" target="_blank" rel="noreferrer">Anchor</a></li><br />
                     <li><a href="https://open.spotify.com/show/7Kqsh8mLHJIlCOIdHXkskT" target="_blank" rel="noreferrer">Spotify</a></li><br />
-                    <li><a href="https://podcasts.apple.com/mx/podcast/de-regalos-y-palabras-con-luc%C3%ADa-c%C3%A1rdenas-of-gifts/id1608798314" target="_blank" rel="noreferrer">Apple Pódcasts</a></li><br />
-                    <li><a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy80MWRmNzY3Yy9wb2RjYXN0L3Jzcw" target="_blank" rel="noreferrer">Google Pódcasts</a></li>
+                    <li><a href="https://podcasts.apple.com/mx/podcast/de-regalos-y-palabras-con-luc%C3%ADa-c%C3%A1rdenas-of-gifts/id1608798314" target="_blank" rel="noreferrer">Apple Podcasts</a></li><br />
+                    <li><a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy80MWRmNzY3Yy9wb2RjYXN0L3Jzcw" target="_blank" rel="noreferrer">Google Podcasts</a></li>
                 </ul>
 
                 <img src={postimg2} alt="wiisy-logo" className="profile-pic" id="podcast-logo" />
